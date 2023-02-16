70ce38161c2c96929b64b2539ad9c517c1ef31b3
logeshkumar1995 <logeshkumarsvg@gmail.com>
2023-02-16 13:04:07 +0530

    Adactin

---
+++ src/test/java/org/task/Adactin.java
@@ -0,0 +1,26 @@
+package org.task;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Adactin {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://adactinhotelapp.com/");
+		driver.manage().window().maximize();
+		WebElement txtname=driver.findElement(By.id("username"));
+		WebElement txtpass=driver.findElement(By.id("password"));
+		WebElement btnlogin=driver.findElement(By.id("login"));
+		txtname.sendKeys("logesh");
+		txtpass.sendKeys("lo@12345");
+		btnlogin.click();
+		Thread.sleep(3000);
+		
+	}
+
+}
